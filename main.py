728ef0ca938951e4b504b7660cf2aa0acc5b4958
Ahmed Asaad <107361633+aamya2003@users.noreply.github.com>
2023-08-18 22:06:41 +0300

    a

--- main.py
+++ main.py
@@ -15,7 +15,26 @@ bot = pyrogram.Client("c", api_hash="b53d2d46ddff41ee2353256559b4c4c2", api_id=2
 @bot.on_message(filters=filters.command(['start']))
 def sayHello(api, message):
     bot.send_message(message.chat.id, "Hello")
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return "<b> hello</b>"
+
+def run():
+  app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+
+
+keep_alive()
 
 bot.run()
 

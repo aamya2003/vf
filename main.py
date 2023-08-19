86550a16c5770366172436660d28aee1b32daaa8
Ahmed Asaad <107361633+aamya2003@users.noreply.github.com>
2023-08-19 11:56:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pyrogram
 from pyrogram import filters
 
 
-bot = pyrogram.Client("c", api_hash="b53d2d46ddff41ee2353256559b4c4c2", api_id=23164619, bot_token="6254468029:AAEuWgaIqZXBEG3vTiSS6D09NnU0LMpKmG0")
+bot = pyrogram.Client("c", api_hash="b53d2d46ddff41ee2353256559b4c4c2", api_id=23164619, bot_token="6409008239:AAENDzlZ7dOo1oP_bY6lZZZqq5pQ0vexQ94")
 # bot.start()
 
 @bot.on_message(filters=filters.command(['start']))
